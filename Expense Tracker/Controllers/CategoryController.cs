c4e84512f6b8355f33d8c8c0bbea092086639539
David Aderibigbe <davidaderibigbe47@gmail.com>
2023-09-06 13:50:49 +0100

    Design Form

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -26,26 +26,8 @@ namespace Expense_Tracker.Controllers
                           Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/AddOrEdit
-        public IActionResult AddOrEdit(int id=0)
+        public IActionResult AddOrEdit(int id = 0)
         {
             if (id == 0)
                 return View(new Category());
@@ -73,26 +55,6 @@ namespace Expense_Tracker.Controllers
             return View(category);
         }
 
-       
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -112,9 +74,9 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
+        //private bool CategoryExists(int id)
+        //{
+        //  return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+        //}
     }
 }
