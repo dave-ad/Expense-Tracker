28179bbac20a2fd25093be900e7b8d8d8c3044dc
David Aderibigbe <davidaderibigbe47@gmail.com>
2023-09-11 00:43:02 +0100

    Recent Transaction Widget

--- Expense Tracker/Controllers/DashBoardController.cs
+++ Expense Tracker/Controllers/DashBoardController.cs
@@ -95,7 +95,12 @@ namespace Expense_Tracker.Controllers
                                           expense = expense == null ? 0 : expense.expense,
                                       };
 
-
+            //Recent Transactions
+            ViewBag.RecentTransactions = await _context.Transactions
+                .Include(i => i.Category)
+                .OrderByDescending(j => j.Date)
+                .Take(5)
+                .ToListAsync();
 
             return View();
         }
